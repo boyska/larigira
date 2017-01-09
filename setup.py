3b49ae06795c3928689ea7ffd60676da63975d04
boyska <piuttosto@logorroici.org>
2017-01-09 16:39:13 +0100

    Release 1.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 setup(name='larigira',
-      version='1.0b1',
+      version='1.0',
       description='A radio automation based on MPD',
       long_description=read('README.rst'),
       author='boyska',
